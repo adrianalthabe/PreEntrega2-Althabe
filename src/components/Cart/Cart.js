9a600d1192352b263e8b5dc27468ce4a4554c7db
adrianalthabe <adrianalthabe@gmail.com>
2023-09-01 01:58:17 -0300

    Se agrega Estilo Css a Cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { CartContext } from "../../context/CartContext";
 import CartItem from "../CartItem/CartItem";
 import { Link } from "react-router-dom";
+import "./Cart.css";
 
 const Cart = () => {
   const { cart, clearCart, totalQuantity, total } = useContext(CartContext);
